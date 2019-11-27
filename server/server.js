d76fd1102b27a67f8aaf6201778c45c968ccc4c6
Jean Pierre <vcjpierre@gmail.com>
2019-11-26 23:56:36 -0400

    Created owners API

--- server/server.js
+++ server/server.js
@@ -28,9 +28,11 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // API
 const productRoutes = require('./routes/product');
 const categoryRoutes = require('./routes/category');
+const ownerRoutes = require('./routes/owner');
 
 app.use('/api', productRoutes);
 app.use('/api', categoryRoutes);
+app.use('/api', ownerRoutes);
 
 app.listen(3000, (err) => {
     if (err) {
