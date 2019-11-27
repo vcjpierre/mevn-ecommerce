47981a33c0fb2669adfc8a5a3eaf58e397b4b270
Jean Pierre <vcjpierre@gmail.com>
2019-11-27 00:14:17 -0400

    Product API: get a single product

--- server/routes/product.js
+++ server/routes/product.js
@@ -27,10 +27,36 @@ router.post('/products', upload.single("photo"), async (req, res) => {
 });
 
 // GET request -> get all product
-
+router.get('/products', async (req, res) => {
+    try {
+        let products = await Product.find();
+        res.json({
+            success: true,
+            products: products
+        });
+    } catch (err) {
+        res.status(500).json({
+            success: false,
+            message: err.message
+        });
+    }
+});
 
 // GET request -> get a single product
-
+router.get('/products/:id', async (req, res) => {
+    try {
+        let product = await Product.findOne({ _id: req.params.id });
+        res.json({
+            success: true,
+            products: product
+        });
+    } catch (err) {
+        res.status(500).json({
+            success: false,
+            message: err.message
+        });
+    }
+});
 
 // PUT request -> update a single product
 
