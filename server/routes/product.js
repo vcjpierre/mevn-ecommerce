b45eb002ab9705097bd7399f17cb7f248ba9b9a6
Jean Pierre <vcjpierre@gmail.com>
2019-11-27 00:37:20 -0400

    Product API: update and delete products

--- server/routes/product.js
+++ server/routes/product.js
@@ -59,8 +59,53 @@ router.get('/products/:id', async (req, res) => {
 });
 
 // PUT request -> update a single product
+router.put('/products/:id', upload.single("photo"), async (req, res) => {
+    try {
+        let product = await Product.findOneAndUpdate(
+            { _id: req.params.id }, 
+            {
+                $set: {
+                    title: req.body.title,
+                    price: req.body.price,
+                    category: req.body.categoryID,
+                    photo: req.file.location,
+                    description: req.body.description,
+                    owner: req.body.ownerID
+                }
+            }, 
+            { upsert: true }
+        );
 
+        res.json({
+            success: true,
+            updatedProduct: product
+        });
+
+    } catch (err) {
+        res.status(500).json({
+            success: false,
+            message: err.message
+        });
+    }
+});
 
 // DELETE request -> delete a single product
+router.delete('/products/:id', async (req, res) => {
+    try {
+        let deletedProduct = await Product.findOneAndDelete({ _id: req.params.id });
+
+        if (deletedProduct) {
+            res.json({
+                status: true,
+                message: "Successfully deleted."
+            });
+        }
+    } catch (err) {
+        res.status(500).json({
+            success: false,
+            message: err.message
+        });
+    }
+});
 
 module.exports = router;
