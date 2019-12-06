5d0b5186f2b9c8cf3ab8039a7a4df8bc26a87e21
Jean Pierre <vcjpierre@gmail.com>
2019-12-06 14:53:47 -0400

    Configure static files

--- admin/nuxt.config.js
+++ admin/nuxt.config.js
@@ -12,7 +12,9 @@ export default {
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: '/css/all.css' },
+      { rel: 'stylesheet', href: '/css/default.css' }
     ]
   },
   /*
