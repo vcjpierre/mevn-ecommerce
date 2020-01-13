8f915bccaf050d1a9de001e578fa4937804c466c
Jean Pierre Velez <vcjpierre@gmail.com>
2020-01-13 16:37:34 -0400

    Update owner.js

--- server/routes/owner.js
+++ server/routes/owner.js
@@ -1,40 +1,44 @@
-const router = require('express').Router()
-const Owner = require('../models/owner');
+const router = require("express").Router();
+const Owner = require("../models/owner");
+const upload = require("../middlewares/upload-photo");
 
-// POST api
-router.post('/owners', async (req, res) => {
-    try {
-        let owner = new Owner();
-        owner.name = req.body.name;
-        owner.about = req.body.about;
-        await owner.save();
+// POST API
+router.post("/owners", upload.single("photo"), async (req, res) => {
+  try {
+    console.log(req.body);
+    let owner = new Owner();
+    owner.name = req.body.name;
+    owner.about = req.body.about;
+    owner.photo = req.file.location;
+    await owner.save();
 
-        res.json({
-            success: true,
-            message: "Successfully created a new owner."
-        });
-    } catch (err) {
-        res.status(500).json({
-            success: false,
-            message: err.message
-        });
-    }
+    res.json({
+      success: true,
+      message: "Successfully created a new owner"
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      success: false,
+      message: err.message
+    });
+  }
 });
 
 // GET api
-router.get('/owners', async (req, res) => {
-    try {
-        let owners = await Owner.find();
+router.get("/owners", async (req, res) => {
+  try {
+    let owners = await Owner.find();
 
-        res.json({
-            owners: owners
-        });
-    } catch (err) {
-        res.status(500).json({
-            success: false,
-            message: err.message
-        });
-    }
+    res.json({
+      owners: owners
+    });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: err.message
+    });
+  }
 });
 
 module.exports = router;
