305184381e0f1ff6b03dcf47f818bd68614ed633
Jean Pierre Velez <vcjpierre@gmail.com>
2020-01-13 01:49:48 -0400

    Update nuxt.config

--- admin/nuxt.config.js
+++ admin/nuxt.config.js
@@ -13,7 +13,7 @@ export default {
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { rel: 'stylesheet', href: '/css/all.css' },
+      { rel: 'stylesheet', href: '/css/font-awesome/css/all.css' },
       { rel: 'stylesheet', href: '/css/default.css' }
     ]
   },
