c59c0d33aa27348f91dc1226bc9c5249aaeac339
Jean Pierre Velez <vcjpierre@gmail.com>
2020-01-14 00:28:05 -0400

    Added navigation links

--- admin/pages/index.vue
+++ admin/pages/index.vue
@@ -1,38 +1,40 @@
 <template>
   <main>
     <div class="a-spacing-large"></div>
-    
-    <div class="container-fluid browsing-history">
-        <div class="row">
-              <div class="col-sm-8 col-8">
-                <h1 class="a-size-large a-spacing-none a-text-normal">All products</h1>
-                <div class="a-spacing-large"></div>
-                <!-- Button -->
-                <a href="#" class="a-button-buy-again">Add a new product</a>
-                <a href="#" class="a-button-history margin-right-10">Add a new category</a>
-                <a href="#" class="a-button-history margin-right-10">Add a new owner</a>
-            </div>
-             <!-- Listing page -->
-        </div>
-    </div>
-   <div class="a-spacing-large"></div>
     <div class="container-fluid browsing-history">
       <div class="row">
-        <div v-for="(product, index) in products" 
-        :key="product._id" class="col-xl-2 col-lg-2 col-md-3 col-sm-6 col-6 br bb">
+        <div class="col-sm-8 col-8">
+          <h1 class="a-size-large a-spacing-none a-text-normal">All products</h1>
+          <div class="a-spacing-large"></div>
+          <!-- Button -->
+          <nuxt-link to="/products" class="a-button-buy-again">Add a new product</nuxt-link>
+          <nuxt-link to="/category" class="a-button-history margin-right-10">Add a new category</nuxt-link>
+          <nuxt-link to="/owner" class="a-button-history margin-right-10">Add a new owner</nuxt-link>
+          <!-- Listing page -->
+        </div>
+      </div>
+    </div>
+    <div class="a-spacing-large"></div>
+    <div class="container-fluid browsing-history">
+      <div class="row">
+        <div
+          v-for="(product, index) in products"
+          :key="product._id"
+          class="col-xl-2 col-lg-2 col-md-3 col-sm-6 col-6 br bb"
+        >
           <div class="history-box">
             <!-- Product image -->
             <a href="#" class="a-link-normal">
-                <img :src="product.photo" alt="" class="img-fluid" />
-              </a>
-              <!-- Product title -->
-              <div class="a-spacing-top-base asin-title">
-                <span class="a-text-normal">
-                  <div class="p13n-sc-truncated">{{ product.title }}</div>
-                </span>
-              </div>
-              <!-- Product rating -->
-              <div class="a-row">
+              <img :src="product.photo" class="img-fluid" />
+            </a>
+            <!-- Product title -->
+            <div class="a-spacing-top-base asin-title">
+              <span class="a-text-normal">
+                <div class="p13n-sc-truncated">{{ product.title }}</div>
+              </span>
+            </div>
+            <!-- Product rating -->
+            <div class="a-row">
               <a href="#">
                 <i class="fas fa-star"></i>
                 <i class="fas fa-star"></i>
@@ -46,13 +48,21 @@
             <!-- Product price -->
             <div class="a-row">
               <span class="a-size-base a-color-price">
-                <span class="p13n-sc-price">$ {{ product.price }}</span>
+                <span class="p13n-sc-price">${{ product.price }}</span>
               </span>
             </div>
-            <!-- Product button -->
+            <!-- Product Buttons -->
             <div class="a-row">
-              <a href="#" class="a-button-history margin-right-10">Update</a>
-              <a href="#" class="a-button-history margin-right-10">Delete</a>
+              <nuxt-link
+                :to="`/products/${product._id}`"
+                class="a-button-history margin-right-10"
+              >Update</nuxt-link>
+              <a
+                href="#"
+                class="a-button-history margin-right-10"
+                @click="onDeleteProduct(product._id, index)">
+                Delete
+              </a>
             </div>
           </div>
         </div>
@@ -63,6 +73,8 @@
 
 <script>
 export default {
+  // asyncData is fetching Data before nuxt page finished loading on the browser.
+  // It is good for SEO because the data will be loaded first
   async asyncData({ $axios }) {
     try {
       let response = await $axios.$get("http://localhost:3001/api/products");
@@ -70,11 +82,24 @@ export default {
       return {
         products: response.products
       };
-    } catch (err) {
-      
+    } catch (err) {}
+  },
+  methods: {
+    async onDeleteProduct(id, index) {
+      try {
+        let response = await this.$axios.$delete(
+          `http://localhost:3001/api/products/${id}`
+        );
+        if (response.status) {
+          // remove
+          this.products.splice(index, 1);
+        }
+      } catch (err) {
+        console.log(err);
+      }
     }
   }
-}
+};
 </script>
 
 <style>
