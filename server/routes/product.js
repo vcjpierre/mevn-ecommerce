41bc8b06250543effc6f1ac0074b84d08a86c9b8
Jean Pierre Velez <vcjpierre@gmail.com>
2020-01-17 14:51:19 -0400

    Update product.js

--- server/routes/product.js
+++ server/routes/product.js
@@ -2,38 +2,11 @@ const router = require('express').Router();
 const Product = require('../models/product');
 const upload = require("../middlewares/upload-photo");
 
-// POST request -> create a new product
-router.post("/products", upload.single("photo"), async (req, res) => {
-    try {
-        let product = new Product();
-        product.ownerID = req.body.ownerID;
-        product.categoryID = req.body.categoryID;
-        product.price = req.body.price;
-        product.title = req.body.title;
-        product.description = req.body.description;
-        product.photo = req.file.location;
-        product.stockQuantity = req.body.stockQuantity;
-
-        await product.save(); // async
-
-        res.json({
-            status: true,
-            message: "Successfully saved"
-        });
-    } catch (err) {
-        res.status(500).json({
-            success: false,
-            message: err.message
-        });
-    }
-});
-
 // GET requesst - get all products
 router.get("/products", async (req, res) => {
     try {
         let products = await Product.find()
             .populate("owner category")
-            .populate("reviews", "rating")
             .exec();
         res.json({
             success: true,
@@ -54,11 +27,36 @@ router.get("/products/:id", async (req, res) => {
                 _id: req.params.id
             })
             .populate("owner category")
-            .populate("reviews", "rating")
             .exec();
         res.json({
             success: true,
             product: product
+        });
+    } catch (err) {
+        res.status(500).json({
+            success: false,
+            message: err.message
+        });
+    }
+});
+
+// POST request -> create a new product
+router.post("/products", upload.single("photo"), async (req, res) => {
+    try {
+        let product = new Product();
+        product.ownerID = req.body.ownerID;
+        product.categoryID = req.body.categoryID;
+        product.price = req.body.price;
+        product.title = req.body.title;
+        product.description = req.body.description;
+        product.photo = req.file.location;
+        product.stockQuantity = req.body.stockQuantity;
+
+        await product.save(); // async
+
+        res.json({
+            status: true,
+            message: "Successfully saved"
         });
     } catch (err) {
         res.status(500).json({
