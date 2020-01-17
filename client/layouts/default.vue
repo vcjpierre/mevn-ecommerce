31157b9681a1d85b0aad92f9091c4f7ac42c8ae9
Jean Pierre Velez <vcjpierre@gmail.com>
2020-01-17 13:28:42 -0400

    Created Footer and Navbar components

---
+++ client/layouts/default.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <Navbar />
+    <nuxt />
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Navbar from "~/components/Navbar";
+import Footer from "~/components/Footer";
+export default {
+  components: {
+    Navbar,
+    Footer
+  }
+};
+</script>
+
+<style>
+body {
+  font-size: 13px;
+  line-height: 19px;
+  color: #111;
+}
+</style>
---
+++ client/pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+    <div>
+      <h1 class="title">
+        Client
+      </h1>
+      <h2 class="subtitle">
+        My exceptional Nuxt.js project
+      </h2>
+    </div>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
