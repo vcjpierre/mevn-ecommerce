3bd592f930b574a857529e6dbed46ad000aca337
Jean Pierre Velez <vcjpierre@gmail.com>
2020-01-24 16:54:00 -0400

    Create JWT middleware

---
+++ server/middlewares/verify-token.js
@@ -0,0 +1,30 @@
+const jwt = require("jsonwebtoken");
+
+module.exports = function (req, res, next) {
+  let token = req.headers["x-access-token"] || req.headers["authorization"];
+  let checkBearer = "Bearer ";
+
+  if (token) {
+    if (token.startsWith(checkBearer)) {
+      token = token.slice(checkBearer.length, token.length);
+    }
+
+    jwt.verify(token, process.env.SECRET, (err, decoded) => {
+      if (err) {
+        res.json({
+          success: false,
+          message: "Failed to authenticate"
+        });
+      } else {
+        req.decoded = decoded;
+
+        next();
+      }
+    });
+  } else {
+    res.json({
+      success: false,
+      message: "No token Provided"
+    });
+  }
+};
