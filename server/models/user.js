bbf5b4df20907de20e0e24221b87f03856f84ccc
Jean Pierre Velez <vcjpierre@gmail.com>
2020-01-24 16:54:22 -0400

    Update user model

--- server/models/user.js
+++ server/models/user.js
@@ -1,11 +1,39 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const bcrypt = require("bcrypt-nodejs");
 
 const UserSchema = new Schema({
-    name: String,
-    email: { type: String, unique: true, required: true },
-    password: { type: String, required: true },
-    address: { type: Schema.Types.ObjectId, ref: "Address" }
+  name: String,
+  email: { type: String, unique: true, required: true },
+  password: { type: String, required: true },
+  address: { type: Schema.Types.ObjectId, ref: "Address" }
 });
+
+UserSchema.pre("save", function(next) {
+  let user = this;
+  if (this.isModified("password") || this.isNew) {
+    bcrypt.genSalt(10, function(err, salt) {
+      if (err) {
+        return next(err);
+      }
+
+      bcrypt.hash(user.password, salt, null, function(err, hash) {
+        if (err) {
+          return next(err);
+        }
+
+        user.password = hash;
+        next();
+      });
+    });
+  } else {
+    return next();
+  }
+});
+
+UserSchema.methods.comparePassword = function(password, next) {
+  let user = this;
+  return bcrypt.compareSync(password, user.password);
+};
 
 module.exports = mongoose.model("User", UserSchema);
