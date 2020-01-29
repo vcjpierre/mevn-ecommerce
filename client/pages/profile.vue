f13cba08b9dd7ab882604ccc99d7ef973239599a
Jean Pierre Velez <vcjpierre@gmail.com>
2020-01-29 00:29:37 -0400

    Created user profile page

---
+++ client/pages/profile.vue
@@ -0,0 +1,90 @@
+<template>
+  <main>
+    <div class="container-fluid c-section">
+      <div class="row">
+        <div class="col-sm-3"></div>
+        <div class="col-sm-6">
+          <div class="a-spacing-top-medium"></div>
+          <h2>Profile Page</h2>
+          <a href="#" @click="onLogout">Logout</a>
+          <form>
+            <!-- Name -->
+            <div class="a-spacing-top-medium">
+              <label>Name</label>
+              <input
+                class="a-input-text"
+                style="width: 100%"
+                v-model="name"
+                :placeholder="$auth.$state.user.name"
+              />
+            </div>
+
+            <!-- Email -->
+            <div class="a-spacing-top-medium">
+              <label>Email</label>
+              <input
+                class="a-input-text"
+                style="width: 100%"
+                v-model="email"
+                :placeholder="$auth.$state.user.email"
+              />
+            </div>
+
+            <!-- Password -->
+            <div class="a-spacing-top-medium">
+              <label>Password</label>
+              <input class="a-input-text" style="width: 100%" v-model="password" />
+            </div>
+
+            <!-- Button -->
+            <hr />
+            <div class="a-spacing-top-large">
+              <span class="a-button-register">
+                <span class="a-button-inner">
+                  <span class="a-button-text" @click="onUpdateProfile">Update Profile</span>
+                </span>
+              </span>
+            </div>
+          </form>
+          <br />
+        </div>
+        <div class="col-sm-3"></div>
+      </div>
+    </div>
+  </main>
+</template>
+    
+<script>
+export default {
+  data() {
+    return {
+      name: "",
+      email: "",
+      password: ""
+    };
+  },
+  methods: {
+    async onUpdateProfile() {
+      let data = {
+        name: this.name,
+        email: this.email,
+        password: this.password
+      };
+      try {
+        let response = await this.$axios.$put("/api/auth/user", data);
+        if (response.success) {
+          this.name = "";
+          this.email = "";
+          this.password = "";
+          await this.$auth.fetchUser();
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    },
+    async onLogout() {
+      await this.$auth.logout();
+    }
+  }
+};
+</script>
