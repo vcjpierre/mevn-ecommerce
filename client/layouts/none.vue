649019693d74653a365322b14b14b42b33356df6
Jean Pierre Velez <vcjpierre@gmail.com>
2020-01-29 00:29:05 -0400

    Create default layout without navigation

---
+++ client/layouts/none.vue
@@ -0,0 +1,18 @@
+  
+<template>
+  <div>
+    <nuxt />
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+body {
+  font-size: 13px;
+  line-height: 19px;
+  color: #111;
+}
+</style>
