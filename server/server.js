56f12ff89e761ac5ab20c567e2d93734c82e2b7d
Jean Pierre Velez <vcjpierre@gmail.com>
2020-02-07 13:29:26 -0400

    Update server routes

--- server/server.js
+++ server/server.js
@@ -1,46 +1,53 @@
-const express = require('express');
-const morgan = require('morgan');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-const cors = require('cors')
+const express = require("express");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
 
-const User = require('./models/user');
+const User = require("./models/user");
 
 dotenv.config();
 
 const app = express();
 
-mongoose.connect(process.env.DATABASE, 
-{ useUnifiedTopology: true, useNewUrlParser: true  }, 
- err => {
-    if (err) {
-        console.log(err)
-    } else {
-        console.log("Connected to the database.");
+mongoose.connect(
+    process.env.DATABASE, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    },
+    err => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log("Connected to the database");
+        }
     }
-});
+);
 
 // Middlewares
 app.use(cors());
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 
-// API
-const productRoutes = require('./routes/product');
-const categoryRoutes = require('./routes/category');
-const ownerRoutes = require('./routes/owner');
-const userRoutes = require('./routes/auth');
+const productRoutes = require("./routes/product");
+const categoryRoutes = require("./routes/category");
+const ownerRoutes = require("./routes/owner");
+const userRoutes = require("./routes/auth");
+const reviewRoutes = require("./routes/review");
 
-app.use('/api', productRoutes);
-app.use('/api', categoryRoutes);
-app.use('/api', ownerRoutes);
-app.use('/api', userRoutes);
+app.use("/api", productRoutes);
+app.use("/api", categoryRoutes);
+app.use("/api", ownerRoutes);
+app.use("/api", userRoutes);
+app.use("/api", reviewRoutes);
 
-app.listen(3001, (err) => {
+app.listen(3001, err => {
     if (err) {
-        console.log(err)
+        console.log(err);
     } else {
         console.log("Listening on PORT", 3001);
     }
