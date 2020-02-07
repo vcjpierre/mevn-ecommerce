96c6627776ae588004d11468e33dd58542788d78
Jean Pierre Velez <vcjpierre@gmail.com>
2020-02-07 13:30:21 -0400

    Update product model

--- server/models/product.js
+++ server/models/product.js
@@ -1,15 +1,32 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const ProductSchema = new Schema({
-    category: { type: Schema.Types.ObjectId, ref: 'Category' },
-    owner: { type: Schema.Types.ObjectId, ref: 'Owner' },
+const ProductSchema = new Schema(
+  {
+    category: { type: Schema.Types.ObjectId, ref: "Category" },
+    owner: { type: Schema.Types.ObjectId, ref: "Owner" },
     title: String,
     description: String,
     photo: String,
     price: Number,
     stockQuantity: Number,
-    rating: [Number]
+    reviews: [{ type: Schema.Types.ObjectId, ref: "Review" }]
+  },
+  {
+    toObject: { virtuals: true },
+    toJSON: { virtuals: true }
+  }
+);
+
+ProductSchema.virtual("averageRating").get(function() {
+  if (this.reviews.length > 0) {
+    let sum = this.reviews.reduce((total, review) => {
+      return total + review.rating;
+    }, 0);
+    return sum / this.reviews.length;
+  }
+
+  return 0;
 });
 
 module.exports = mongoose.model("Product", ProductSchema);
