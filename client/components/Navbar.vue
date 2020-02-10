3ed485e08826e93495441ff954fdec0fe3249069
Jean Pierre Velez <vcjpierre@gmail.com>
2020-02-10 12:26:51 -0400

    Address city in navbar

--- client/components/Navbar.vue
+++ client/components/Navbar.vue
@@ -26,9 +26,9 @@
                 <div class="nav-sprite" id="nav-packard-glow-loc-icon"></div>
                 <div id="glow-ingress-block">
                   <span class="nav-line-1" id="glow-ingress-line1">Deliver to</span>
-                  <span
-                    class="nav-line-2"
-                    id="glow-ingress-line2">City</span>
+                  <span class="nav-line-2" id="glow-ingress-line2">
+                    {{ $auth.$state.user.address.city }}
+                  </span>
                 </div>
               </nuxt-link>
             </div>
