6ea76d016ad4603c2c3ac6c63a02c1ab8c686c7a
Jean Pierre Velez <vcjpierre@gmail.com>
2020-02-11 18:38:54 -0400

    Mutations, getters and computed

---
+++ client/store/index.js
@@ -0,0 +1,91 @@
+export const state = () => ({
+  // State
+  cart: [],
+  cartLength: 0
+});
+
+export const actions = {
+  addProductToCart({ state, commit }, product) {
+    const cartProduct = state.cart.find(prod => prod._id === product._id);
+
+    if (!cartProduct) {
+      commit("pushProductToCart", product);
+    } else {
+      commit("incrementProductQty", cartProduct);
+    }
+
+    commit("incrementCartLength");
+  }
+};
+
+export const mutations = {
+  pushProductToCart(state, product) {
+    product.quantity = 1;
+    state.cart.push(product);
+  },
+
+  incrementProductQty(state, product) {
+    product.quantity++;
+    let indexOfProduct = state.cart.indexOf(product);
+    state.cart.splice(indexOfProduct, 1, product);
+  },
+
+  incrementCartLength(state) {
+    state.cartLength = 0;
+    if (state.cart.length > 0) {
+      state.cart.map(product => {
+        state.cartLength += product.quantity;
+      });
+    }
+  },
+  /*
+  1. Find the product in the cart
+  2. Change the quantity of the product
+  3. Update the length of the cart
+  4. replace the old product with the updated product
+  */
+  changeQty(state, {
+    product,
+    qty
+  }) {
+    let cartProduct = state.cart.find(prod => prod._id === product._id);
+    cartProduct.quantity = qty;
+
+    state.cartLength = 0;
+    if (state.cart.length > 0) {
+      state.cart.map(product => {
+        state.cartLength += product.quantity;
+      });
+    }
+
+    let indexOfProduct = state.cart.indexOf(cartProduct);
+    state.cart.splice(indexOfProduct, 1, cartProduct);
+  },
+  /*
+  1. remove the product quantity from the cartLength
+  2. get the index of the product that we want to delete
+  3. remove that product by using splice
+  */
+  removeProduct(state, product) {
+    state.cartLength -= product.quantity;
+    let indexOfProduct = state.cart.indexOf(product);
+    state.cart.splice(indexOfProduct, 1);
+  }
+};
+
+export const getters = {
+  getCartLength(state) {
+    return state.cartLength;
+  },
+  getCart(state) {
+    return state.cart;
+  },
+  getCartTotalPrice(state) {
+    let total = 0;
+    state.cart.map(product => {
+      total += product.price * product.quantity;
+    });
+
+    return total;
+  }
+};
