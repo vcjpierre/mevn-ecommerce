8660efe8757787531f5b39dcdeb711a87ca794a8
Jean Pierre Velez <vcjpierre@gmail.com>
2020-02-13 16:30:31 -0400

    Create order API

--- server/server.js
+++ server/server.js
@@ -42,6 +42,7 @@ const userRoutes = require("./routes/auth");
 const reviewRoutes = require("./routes/review");
 const addressRoutes = require("./routes/address");
 const paymentRoutes = require("./routes/payment");
+const orderRoutes = require("./routes/order");
 
 app.use("/api", productRoutes);
 app.use("/api", categoryRoutes);
@@ -50,6 +51,7 @@ app.use("/api", userRoutes);
 app.use("/api", reviewRoutes);
 app.use("/api", addressRoutes);
 app.use("/api", paymentRoutes);
+app.use("/api", orderRoutes);
 
 app.listen(3001, err => {
     if (err) {
