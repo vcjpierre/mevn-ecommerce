b785da86cd6e946a0fc25c3f11da9672eac5a293
Jean Pierre Velez <vcjpierre@gmail.com>
2020-02-13 16:08:48 -0400

    POST payment

--- server/routes/payment.js
+++ server/routes/payment.js
@@ -1,5 +1,8 @@
 const router = require("express").Router();
 const moment = require("moment");
+const stripe = require("stripe")(process.env.STRIPE_SECREY_KEY);
+const verifyToken = require("../middlewares/verify-token");
+const Order = require("../models/order");
 
 const SHIPMENT = {
   normal: {
@@ -29,6 +32,53 @@ router.post("/shipment", (req, res) => {
   }
 
   res.json({ success: true, shipment: shipment });
+});
+
+router.post("/payment", verifyToken, (req, res) => {
+  let totalPrice = Math.round(req.body.totalPrice * 100);
+  stripe.customers
+    .create({
+      email: req.decoded.email
+    })
+    .then(customer => {
+      return stripe.customers.createSource(customer.id, {
+        source: "tok_visa"
+      });
+    })
+    .then(source => {
+      return stripe.charges.create({
+        amount: totalPrice,
+        currency: "usd",
+        customer: source.customer
+      });
+    })
+    .then(async charge => {
+      let order = new Order();
+      let cart = req.body.cart;
+
+      cart.map(product => {
+        order.products.push({
+          productID: product._id,
+          quantity: parseInt(product.quantity),
+          price: product.price
+        });
+      });
+
+      order.owner = req.decoded._id;
+      order.estimatedDelivery = req.body.estimatedDelivery;
+      await order.save();
+
+      res.json({
+        success: true,
+        message: "Successfully made a payment"
+      });
+    })
+    .catch(err => {
+      res.status(500).json({
+        success: false,
+        message: err.message
+      });
+    });
 });
 
 module.exports = router;
